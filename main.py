234a07af3911723e2406af960169ae6d80956e0b
Amir Abedi <abedimhosein@gmail.com>
2025-05-25 12:49:03 +0330

    clean project files

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from app_logic import VideoSchedulerAppLogic
 from database import create_db_and_tables
-from .gui import VideoSchedulerGUI
+from gui import VideoSchedulerGUI
 
 if __name__ == "__main__":
     create_db_and_tables()  # Ensure database and tables exist on startup
